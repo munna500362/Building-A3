8288a48b46064e8b6570a49de8b3e7586e5c889b
mehedi hasan <mehedihasan500362@gmail.com>
2025-03-02 05:59:10 +0600

    Commercial div commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html", 
+      "./src/**/*.{js,ts,jsx,tsx}" // Ekhane tumi nijer project er file path add korte paro
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
